240cfcfffb6f9ba344f7e1b487fc4494dfd26948
Yunus Eren Guzel <exculuber@gmail.com>
2015-07-28 16:23:41 +0300

    configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ Workshop.create_product(
   saloon: 'SALON B',
   moderator: 'Yrd. Doç. Dr. Mehmet Palancı',
   price: 10000,
+  stock:20,
   start_at: '2015-10-24 14:30:00 +0300',
   finish_at: '2015-10-24 16:00:00 +0300'
 )
@@ -18,6 +19,7 @@ Workshop.create_product(
   name:'Üstün Yetenekli Çocuklar için İlkokul ve/veya Ortaokul düzeyinde Matematikte Iraksak ve Yakınsak Problem Uygulamaları',
   saloon: 'SALON B',
   moderator: 'Dr. Burak Karabey',
+  stock:20,
   price: 10000,
   start_at: '2015-10-24 16:00:00 +0300',
   finish_at: '2015-10-24 17:30:00 +0300'
@@ -26,6 +28,7 @@ Workshop.create_product(
   name:'Ahşap Atölyesi',
   saloon: 'SALON B',
   moderator: 'Gilika Çocuk Tasarım Atölyesi',
+  stock:20,
   price: 18000,
   start_at: '2015-10-24 14:30:00 +0300',
   finish_at: '2015-10-24 17:30:00 +0300'
@@ -34,6 +37,7 @@ Workshop.create_product(
   name:'Erken Yaştaki Üstün Potansiyelli Çocuklara Erken Müdahale Yöntemleri ve Uygulamaları',
   saloon: 'SALON B',
   moderator: 'Gilika Çocuk Tasarım Atölyesi',
+  stock:20,
   price: 10000,
   start_at: '2015-10-25 10:30:00 +0300',
   finish_at: '2015-10-25 12:00:00 +0300'
@@ -42,6 +46,7 @@ Workshop.create_product(
   name:'Çocuklarda Yaratıcı Hareket Yoluyla Düşünme ve Problem Çözme Becerilerini Geliştirme Atölyesi',
   saloon: 'SALON B',
   moderator: 'Gilika Çocuk Tasarım Atölyesi',
+  stock:20,
   price: 10000,
   start_at: '2015-10-25 12:00:00 +0300',
   finish_at: '2015-10-25 13:30:00 +0300'
@@ -50,6 +55,7 @@ Workshop.create_product(
   name:'Kâğıt Atölyesi – Origami & Krigami',
   saloon: 'SALON B',
   moderator: 'Gilika Çocuk Tasarım Atölyesi',
+  stock:20,
   price: 18000,
   start_at: '2015-10-25 10:30:00 +0300',
   finish_at: '2015-10-25 13:30:00 +0300'
@@ -58,11 +64,13 @@ Workshop.create_product(
   name:'Zekâ Oyunları ve Oyuncakları Tasarımı Atölyesi',
   saloon: 'SALON B',
   moderator: 'Gilika Çocuk Tasarım Atölyesi',
+  stock:20,
   price: 10000,
   start_at: '2015-10-25 13:30:00 +0300',
   finish_at: '2015-10-25 16:30:00 +0300'
 )
 Attendance.create_product(
   price: 18000,
-  name:'Zeka Kongresi Katılım'
+  name:'Zeka Kongresi Katılım',
+  stock:900
 )
