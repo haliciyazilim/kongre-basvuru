ad249fda292dc22692469b49ec776edfbce4f7b4
Yunus Eren Guzel <exculuber@gmail.com>
2015-08-03 17:14:13 +0300

    daily group by

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,6 +10,7 @@ class AdminController < ApplicationController
       @receipts = Receipt.all.order(id: :desc)
     end
     @total_amount = @receipts.map(&:price).reduce(:+)
+    @receipts = @receipts.to_ary.group_by{|r| "#{r.created_at.day}.#{r.created_at.month}.#{r.created_at.year}"}
   end
   def applicant
     @applicant = Applicant.find(params[:id])
