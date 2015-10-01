ff7e0807080982d46fcb79593aa3bcac3774346f
Eren Halici <eren@halici.com.tr>
2015-10-01 16:08:55 +0300

    Reverted name

--- app/mailers/kongre_mailer.rb
+++ app/mailers/kongre_mailer.rb
@@ -1,5 +1,5 @@
 class KongreMailer < ActionMailer::Base
-  default from: 'Türkiye Zeka Vakfı <info@tzv.org.tr>'
+  default from: 'info@tzv.org.tr'
   def payment_accepted(receipt)
     @receipt = receipt
     mail(to:receipt.applicant.email, subject: 'Ödemeniz Onaylandı')
