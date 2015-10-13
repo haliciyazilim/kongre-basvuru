a389e587890710f056ddacad5b818fa39c8eefea
Eren Halici <eren@halici.com.tr>
2015-10-13 18:01:22 +0300

    Showing distinct emails of unpaid users

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -1,14 +1,21 @@
+
 class Admin::ApplicantsController < AdminController
   def unpaid
     sql = <<-SQL
-      SELECT * FROM
-      ( SELECT applicants.tckn FROM (Applicants
-        INNER JOIN receipts
-        ON applicants.id=receipts.applicant_id)
+      SELECT DISTINCT email from
+      ( SELECT applicants.tckn FROM
+        ( SELECT applicants.tckn FROM
+            (Applicants
+            INNER JOIN receipts
+            ON applicants.id=receipts.applicant_id)
+          GROUP BY applicants.tckn
+          HAVING COUNT(CASE WHEN receipts.is_paid=true THEN 1 END) < 1
+        ) t
+        JOIN applicants ON applicants.tckn = t.tckn
         GROUP BY applicants.tckn
-        HAVING COUNT(CASE WHEN receipts.is_paid=true THEN 1 END) < 1 ) t
-      JOIN applicants ON applicants.tckn = t.tckn
-      ORDER BY applicants.tckn;
+        HAVING COUNT(CASE WHEN applicants.applicant_type='presenter' THEN 1 END) < 1
+      ) t2
+      JOIN applicants ON applicants.tckn = t2.tckn;
     SQL
 
     @applicants = ActiveRecord::Base.connection.execute(sql)
