d8a393e1fb12314612254fa43594b5c0c31ba8cf
emre <emre@halici.com.tr>
2015-11-04 12:59:13 +0200

    Merge branch 'master' of C:\halici\kongre\kongre-basvuru with conflicts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_action :before_action
+
   def before_action
     response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
+
   def show
     @workshops_24 = Workshop.at_day '2015-10-24'
     @workshops_25 = Workshop.at_day '2015-10-25'
@@ -19,33 +21,33 @@ class ApplicationController < ActionController::Base
     if params[:applicant]
       applicant = Applicant.create if !applicant
       session[:applicant_type] = applicant.update(
-        params[:applicant].permit(
-          :name,
-          :surname,
-          :email,
-          :tckn,
-          :birthday,
-          :phone,
-          :organization,
-          :occupation,
-          :address,
-          :city,
-          :relation_to_high_intelligence,
-          :previous_attendances,
-          :applicant_category,
-          :applicant_type
-        ))
+          params[:applicant].permit(
+              :name,
+              :surname,
+              :email,
+              :tckn,
+              :birthday,
+              :phone,
+              :organization,
+              :occupation,
+              :address,
+              :city,
+              :relation_to_high_intelligence,
+              :previous_attendances,
+              :applicant_category,
+              :applicant_type
+          ))
     end
 
     if params[:presentation] && applicant
       presentation = ApplicantPresentation.find_by_applicant_id(applicant.id)
-      presentation = ApplicantPresentation.create(applicant_id:applicant.id) if ! presentation
+      presentation = ApplicantPresentation.create(applicant_id: applicant.id) if !presentation
       presentation.update(
-        params[:presentation].permit(
-          :purpose,
-          :content,
-          :audience
-        ))
+          params[:presentation].permit(
+              :purpose,
+              :content,
+              :audience
+          ))
     end
 
     render :json => applicant
@@ -54,34 +56,32 @@ class ApplicationController < ActionController::Base
   def order
     applicant = Applicant.find(params[:applicant_id])
     ActiveRecord::Base.transaction do
-      receipt = Receipt.create(applicant:applicant)
-      if applicant.applicant_category != ApplicantCategory.child
-        ReceiptProduct.create(
-          receipt:receipt,
-          product:Attendance.last.product,
-          price:applicant.applicant_category == ApplicantCategory.instructor_student ? 10000 : 18000
-        )
-      end
+      receipt = Receipt.create(applicant: applicant)
+      ReceiptProduct.create(
+          receipt: receipt,
+          product: Attendance.last.product,
+          price: applicant.applicant_category == ApplicantCategory.instructor_student ? 10000 : 18000
+      )
       if params[:workshops]
         params[:workshops].each do |workshop_id|
           workshop = Workshop.find(workshop_id)
           ReceiptProduct.create(
-            receipt:receipt,
-            product:workshop.product,
-            price:workshop.product.price
+              receipt: receipt,
+              product: workshop.product,
+              price: workshop.product.price
           )
         end
       end
-      receipt.update(price:receipt.calculate_total_amount)
+      receipt.update(price: receipt.calculate_total_amount)
       url = PaymentManager.checkout(
-        user_id: applicant.id,
-        user_name: applicant.name,
-        order_id: receipt.id,
-        product_name: 'Kongre Katılım',
-        price: receipt.price,
-        hostname: request.protocol + request.host_with_port
+          user_id: applicant.id,
+          user_name: applicant.name,
+          order_id: receipt.id,
+          product_name: 'Kongre Katılım',
+          price: receipt.price,
+          hostname: request.protocol + request.host_with_port
       )
-      render json: { redirect_url:url }
+      render json: {redirect_url: url}
     end
   end
 
@@ -91,8 +91,7 @@ class ApplicationController < ActionController::Base
     if @payment['status'] == 'successful'
       receipt = Receipt.find(@payment['order_id'])
       if !receipt.is_paid
-        CardNumber.create(:applicant_id => receipt.applicant_id)
-        receipt.update(is_paid:true)
+        receipt.update(is_paid: true)
         receipt.receipt_products.each do |rp|
           rp.product.decrement!(:stock)
         end
