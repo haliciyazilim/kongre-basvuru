29531a09771a4fe653c60b92dba5345e2290d827
emreakbaba <emre@halici.com.tr>
2016-09-05 12:28:37 +0300

    admin bug-fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -24,9 +24,9 @@ class AdminController < ApplicationController
 
   def receipts
     if params.has_key? :is_paid
-      @receipts = Receipt.includes(:applicant).where(is_paid: params[:is_paid]).where(:applicants => {:season => calculate_season}).order(id: :desc)
+      @receipts = Receipt.includes(:applicant).where(is_paid: params[:is_paid]).where(:applicants => {:season => calculate_season}).where("price is not null").order(id: :desc)
     else
-      @receipts = Receipt.includes(:applicant).where(:applicants => {:season => calculate_season}).order(id: :desc)
+      @receipts = Receipt.includes(:applicant).where(:applicants => {:season => calculate_season}).where("price is not null").order(id: :desc)
     end
     @total_amount = @receipts.map(&:price).reduce(:+)
     @receipts = @receipts.to_ary.group_by { |r| "#{r.created_at.day}.#{r.created_at.month}.#{r.created_at.year}" }
