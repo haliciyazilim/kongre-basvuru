7cad63968727b2c5d2bb0dcd9fdfbe6238e6e686
emreakbaba <emre@halici.com.tr>
2016-09-20 11:33:13 +0300

    email strip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
       params[:applicant][:occupation] = params[:applicant][:occupation].titleize
       params[:applicant][:organization] = params[:applicant][:organization].titleize
       params[:applicant][:address] = params[:applicant][:address].titleize
-      params[:applicant][:email] = params[:applicant][:email].downcase
+      params[:applicant][:email] = params[:applicant][:email].downcase.strip
       session[:applicant_type] = applicant.update(
           params[:applicant].permit(
               :name,
@@ -89,9 +89,9 @@ class ApplicationController < ActionController::Base
         while !Coupon.where(:code => code).blank?
           code = rand(36**8).to_s(36).upcase
         end
-        Coupon.create(:code => code, :amount => 95, :season => calculate_season, :email => params[:email], :coupon_type => 'metu_student')
+        Coupon.create(:code => code, :amount => 95, :season => calculate_season, :email => params[:email].strip, :coupon_type => 'metu_student')
         begin
-          KongreMailer.send_coupon_mail(params[:email], code).deliver!
+          KongreMailer.send_coupon_mail(params[:email].strip, code).deliver!
         rescue
           puts 'Email could not be sent'
         end
@@ -101,10 +101,10 @@ class ApplicationController < ActionController::Base
         while !Coupon.where(:code => code).blank?
           code = rand(36**8).to_s(36).upcase
         end
-        Coupon.create(:code => code, :amount => 120, :season => calculate_season, :email => params[:email], :coupon_type => 'free')
+        Coupon.create(:code => code, :amount => 120, :season => calculate_season, :email => params[:email].strip, :coupon_type => 'free')
 
         begin
-          KongreMailer.send_coupon_mail(params[:email], code).deliver!
+          KongreMailer.send_coupon_mail(params[:email].strip, code).deliver!
         rescue
           puts 'Email could not be sent'
         end
