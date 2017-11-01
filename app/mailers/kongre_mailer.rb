dbbfdebfd420c842b9482bb3b8b15995c4e7dd29
emre <yunusemreakbaba@gmail.com>
2017-11-01 10:12:52 +0300

    attendance info mail subject

--- app/mailers/kongre_mailer.rb
+++ app/mailers/kongre_mailer.rb
@@ -15,7 +15,7 @@ class KongreMailer < ActionMailer::Base
     @card_number = applicant.card_number.id
     @workshops = applicant.paid_workshops.map { |w| w.product.name }
 
-    mail(to: applicant.email, subject: 'Aklını Kullan #KorkmaBitmez')
+    mail(to: applicant.email, subject: 'V. ZEKA ve YETENEK KONGRESİ #kafakafaya')
   end
 
   def send_coupon_mail(email, code)
