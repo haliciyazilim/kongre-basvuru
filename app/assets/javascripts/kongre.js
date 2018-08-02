c4eab4e64e7786bd5d12fd34fd7dc5bedbc77ff4
emre <emre@halici.com.tr>
2018-08-02 10:18:16 +0300

    kongre tickets over

--- app/assets/javascripts/kongre.js
+++ app/assets/javascripts/kongre.js
@@ -13,7 +13,7 @@ kongreApp.controller('registerFormController', ['$scope', '$http', '$document', 
         onAction: 1
     };
 
-    $scope.ticketsOver = false;
+    $scope.ticketsOver = true;
 
     $scope.currentYear = new Date().getFullYear();
     $scope.personalInfoState = $scope.actionState.onIdle;
