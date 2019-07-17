5f68de0280f75394337c4049d5e5fc3a661c6995
emre <emre@halici.com.tr>
2019-07-17 14:42:52 +0300

    fix: dates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < ActionController::Base
   end
 
   def show
-    @workshops_24 = Workshop.at_day '2019-10-4'
-    @workshops_25 = Workshop.at_day '2019-10-5'
+    @workshops_24 = Workshop.at_day('2019-10-4').order(start_at: :asc)
+    @workshops_25 = Workshop.at_day('2019-10-5').order(start_at: :asc)
   end
 
   def register
