f92906ee52379763a8b51846974d16f69eaf60d5
emre <emre@halici.com.tr>
2019-08-19 13:05:50 +0300

    applicants

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :receipt
   has_many :receipt_products
+
+  def applicants
+    r = receipt_products.map{|t| t.receipt if t.receipt.is_paid == true}.compact
+    r.map{|t| {name: "#{t.applicant.name} #{t.applicant.surname}", email: t.applicant.email, phone: t.applicant.phone}}
+  end
 end
