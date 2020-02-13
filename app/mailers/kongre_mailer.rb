d0b0cb35eee4ee146e960dbf55044b4e9a2630bd
Yaşar Utku Dankaz <utku@halici.com.tr>
2020-02-13 13:33:00 +0300

    daily mail

--- app/mailers/kongre_mailer.rb
+++ app/mailers/kongre_mailer.rb
@@ -14,17 +14,19 @@ class KongreMailer < ActionMailer::Base
 
     @card_number = card_number
     @workshops = workshops
-
-    mail(to: email, subject: 'VII. ZEKA ve YETENEK KONGRESİ #BenBilmemBeyinBilir')
+    # mail(to: email, subject: 'VII. ZEKA ve YETENEK KONGRESİ #BenBilmemBeyinBilir')
+    mail(to: email, subject: 'ZEKA VE EĞİTİM ZİRVESİ')
   end
 
   def attendance_info_mail_ncn(email)
-    mail(to: email, subject: 'V. ZEKA ve YETENEK KONGRESİ #kafakafaya')
+    # mail(to: email, subject: 'V. ZEKA ve YETENEK KONGRESİ #kafakafaya')
+    mail(to: email, subject: 'ZEKA VE EĞİTİM ZİRVESİ')
   end
 
   def send_coupon_mail(email, code)
     @code = code
-    mail(:to => email, :subject => 'Kongre Katılım Bedeli İndirim Kuponu')
+    # mail(:to => email, :subject => 'Kongre Katılım Bedeli İndirim Kuponu')
+    mail(:to => email, :subject => 'Zeka ve Eğitim Zirvesi Katılım Bedeli İndirim Kuponu')
   end
 
   def free_order_accepted(receipt)
@@ -34,13 +36,14 @@ class KongreMailer < ActionMailer::Base
 
   def daily_info_mail(email)
     @receipts = Receipt.where(:is_paid => true).where("created_at > ? and created_at < ?", Date.today.beginning_of_day, Date.today.end_of_day)
-    start_of_this_season = Date.new(2019,7,17)
-    start_of_previous_season = Date.new(2018,7,3)
+    start_of_this_season = Date.new(2020,2,13)
+    start_of_previous_season = Date.new(2019,7,17)
     @diff = (Date.today - start_of_this_season).to_i
     @receipts_this_year_sum = Receipt.where(:is_paid => true).where("created_at > ? and created_at < ?", start_of_this_season.beginning_of_day, Date.today.end_of_day).count
     @receipts_previous_year_today = Receipt.where(:is_paid => true).where("created_at > ? and created_at < ?", (start_of_previous_season + @diff.days).beginning_of_day, (start_of_previous_season + @diff.days).end_of_day).count
     @receipts_previous_year_sum = Receipt.where(:is_paid => true).where("created_at > ? and created_at < ?", start_of_previous_season.beginning_of_day, (start_of_previous_season + @diff.days).end_of_day).count
     @total = @receipts.sum(:price) / 100
-    mail(to: email, subject: 'Kongre - Günlük Bilgilendirme')
+    # mail(to: email, subject: 'Kongre - Günlük Bilgilendirme')
+    mail(to: email, subject: 'Zeka ve Eğitim Zirvesi - Günlük Bilgilendirme')
   end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -3,11 +3,12 @@ namespace :schedule do
 
   task :daily_info_mail => :environment do
     puts 'Started sending daily info mail'
-    admins = ['emre@halici.com.tr', 'ezgi@tzv.org.tr', 'emrehan@halici.com.tr', 'eren@halici.com.tr']
+    admins = ['emre@halici.com.tr', 'ezgi@tzv.org.tr', 'emrehan@halici.com.tr', 'eren@halici.com.tr', 'utku@halici.com.tr']
     begin
-      admins.each do |email|
-        KongreMailer.daily_info_mail(email).deliver!
-      end
+      KongreMailer.daily_info_mail(admins).deliver!
+      # admins.each do |email|
+      #   KongreMailer.daily_info_mail(email).deliver!
+      # end
     rescue
       puts "An error occurred during mail sending for daily info"
     end
